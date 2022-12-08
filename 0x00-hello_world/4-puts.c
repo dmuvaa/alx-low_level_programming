6fa46b7bbe4bf5dd8710b49f494ed473c680b383
dmuvaa <dmuvaa70@gmail.com>
2022-12-08 15:47:39 -0800

    putc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,8 @@
 #!/bin/bash
-puts ("Programming is like building a multilingual puzzle")
+
+#include <stdio.h>
+int main (void)
+{	
+	puts ("Programming is like building a multilingual puzzle")
+		return (0)
+}
