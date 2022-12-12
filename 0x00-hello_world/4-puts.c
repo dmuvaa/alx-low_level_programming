e376437c953a260027ce52e459145e02688842e8
dmuvaa <dmuvaa70@gmail.com>
2022-12-12 05:47:17 -0800

    my putsright

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-/*
- * main-entry point
- */
+/**
+*main-entry point
+*Return: Always zero
+**/
 int main(void)
-{
+{	
 	puts("\"Programming is like building a multilingual puzzle\n");
-       	return (0);
-	/*
-	 * returns value to zero
-	 */
+	return (0)
 }
