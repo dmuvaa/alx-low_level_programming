a7a21cb0f10594baca48fb13143f234f1bb823a6
dmuvaa <dmuvaa70@gmail.com>
2022-12-12 07:54:33 -0800

    Sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+*main -Entry Level
+*return: always zero (success)
+**/
+int main(void)
+{
+	printf("%lu\n", sizeof(char));
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(float))
+	printf("%lu\n", sizeof(double))
+	return (0);
+}
