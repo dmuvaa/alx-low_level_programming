d15baae41a1124a32f46a5d8ba4f1726f2d3f476
dmuvaa <dmuvaa70@gmail.com>
2022-12-15 13:59:24 -0800

    tebahpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+#include<stdio>
+/**
+*main - Entry point
+*description- print backwards
+*return: return value after executing to zero
+*/
+int main(void)
+{
+	char = c;
+
+	for(c = 'z'; c >= 'a', c--)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
