01bdd8067ed0b46ccb11deba4955e262fae0ea97
dmuvaa <dmuvaa70@gmail.com>
2022-12-15 11:33:51 -0800

    print all alphas

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include<stdlib.h>
+#include<stdio.h>                                                                                                                       
+/**                                                                                                                                     
+*main- Entry Point                                                                                                                      
+*Description: print alphabet                                                                                                            
+*return: always 0                                                                                                                       
+**/
+int main(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; c++) 
+	{
+	putchar(c); 
+	}
+
+	c = 'A'
+	for (c= 'A'; c <= 'Z'; c++)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);                                                                                                                     
+}
