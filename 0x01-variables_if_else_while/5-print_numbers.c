a000b841c360808119a490201cf56b3087e2339e
dmuvaa <dmuvaa70@gmail.com>
2022-12-15 13:15:20 -0800

    baseten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+*Main - Entry Point
+*Print all to base 
+*Returns: to 0
+*/
+int main(void)
+{
+	int i = '0';
+	for (i = '0'; i <= 9; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
