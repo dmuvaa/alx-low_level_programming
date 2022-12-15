9e96eff0785653e0c9bba306cb71cd02f6144a50
dmuvaa <dmuvaa70@gmail.com>
2022-12-15 13:23:50 -0800

    base ten digits

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,15 @@
 #include<stdlib.h>
 #include<stdio.h>
 /**
-*Main - Entry Point
+*main - Entry Point
 *Print all to base 
-*Returns: to 0
+*return: to 0
 */
 int main(void)
 {
 	int i = '0';
-	for (i = '0'; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
+
 	{
 	putchar(i);
 	}
