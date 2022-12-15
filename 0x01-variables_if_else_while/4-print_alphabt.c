602fcbb1286b77f4d3c1781673370a2d32b35d92
dmuvaa <dmuvaa70@gmail.com>
2022-12-15 12:09:43 -0800

    printfor alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+*main - Entry point
+*Print alphabet minus e and q
+*Return: Return 0
+*/
+int main(void)
+{
+	char c= 'a'
+	for (c = 'a'; c <= 'a'; a++)
+	{
+	putchar(a);
+	}
+
+	putchar("\n');
+	return (0);
+}
