8fe9b805f8769df7a63b599a4bb1801b488f74c5
dmuvaa <dmuvaa70@gmail.com>
2022-12-15 13:39:45 -0800

    numbersprint

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+*main - Entry point
+*execute and return
+*return: return to 0 after executing
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+	putchar(n);
+	}
+	putchar('\n')
+	return (0);
+}
