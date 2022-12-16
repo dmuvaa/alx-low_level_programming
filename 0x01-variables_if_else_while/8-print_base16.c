ef67afc81f49195e736f0125361526278230b640
dmuvaa <dmuvaa70@gmail.com>
2022-12-16 00:46:03 -0800

    print sixteen

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include<stdlid.h>
+#include<stdio.h>
+/**
+*main - main function program
+*Retutn- 0
+**/
+int main(void)
+{
+	char c ='0';
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	putchar(c);
+	}
+
+	c = 'a';
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+	putchar(c);
+	}
+
+	putchar ('\n');
+	return (0);
+}
