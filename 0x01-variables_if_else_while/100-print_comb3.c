090afbf91044f1d3a8a1a099b3b871e186ad5e95
dmuvaa <dmuvaa70@gmail.com>
2022-12-16 02:29:30 -0800

    combotwo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,9 +9,9 @@ int main (void)
 {
 	int a, b;
 
-	for (a = '0'; a < '9'; a++)
+	for (a = '0'; a < '8'; a++)
 	{
-	for (b = a + 1; b < '10'; b++)
+	for (b = a + 1; b < '9'; b++)
 	{
 	if (a != b)
 	{
