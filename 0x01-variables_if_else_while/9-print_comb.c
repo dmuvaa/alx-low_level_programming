0a0d115c4ef653361a678b3e7ba2a3b53136b2f5
dmuvaa <dmuvaa70@gmail.com>
2022-12-16 01:09:05 -0800

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+*main - main function
+*Return: always 0
+*/
+int main(void)
+{
+	int i;
+	for (i = 0; i < 9; i++)
+	{
+	putchar(i);
+	}
+
+	putchar('\n',);
+	return (0);
+}
