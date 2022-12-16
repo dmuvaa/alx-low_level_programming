e8cc4e20557ca1ec6893c33b23166ce04b7f819b
dmuvaa <dmuvaa70@gmail.com>
2022-12-16 01:14:12 -0800

    comb print

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,11 +7,15 @@
 int main(void)
 {
 	int i;
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 	putchar(i);
+	if (i == 9);
+	break
+	putchar(',');
+	putchar(' ');
 	}
 
-	putchar('\n',);
+	putchar('\n');
 	return (0);
 }
