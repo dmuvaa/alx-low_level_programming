352e3954996807d67c76fd28ab58cab0f49de92b
dmuvaa <dmuvaa70@gmail.com>
2022-12-19 09:44:15 -0800

    is loweerrrrr

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - print a function that reads lowercase
+ *
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+	char c;
+	
+	if c islower
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
