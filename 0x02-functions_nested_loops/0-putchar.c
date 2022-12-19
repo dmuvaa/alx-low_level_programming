7b322351270d4ff360c9a867d72d38fc218fb0bb
dmuvaa <dmuvaa70@gmail.com>
2022-12-19 05:47:18 -0800

    puthh

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include<stdio.h>
 #include "main.h"
 /*
  * main - Function Declaration
@@ -7,13 +6,15 @@
 
 int main(void)
 {
-	int i;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	i = _putchar
-	{
-	putchar(i);
-	}
-
-	putchar('\n');
 	return (0);
 }
