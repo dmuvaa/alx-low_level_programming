22fc845e3018b9c8d208951d0276696554d89321
dmuvaa <dmuvaa70@gmail.com>
2022-12-19 06:58:24 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void);
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= z; c++)
+	{
+	_putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
