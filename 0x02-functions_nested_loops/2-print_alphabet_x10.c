ade08191f7aaaf86e4c96318db29bd1c96691528
dmuvaa <dmuvaa70@gmail.com>
2022-12-19 07:25:19 -0800

    nest alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Print alphabets 10 times using the nested loop
+ *
+ * Return: On success 0
+ */
+void print_alphabet_x10(void);
+{
+	char i;
+	char j
+	
+	for( i = 'a'; i <= 'j'; i++)
+	{
+	_putchar(i);
+	}
+	for (j = 10)
+	_putchar('\n')
+}
