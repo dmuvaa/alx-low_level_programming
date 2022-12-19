153c6af3547f8b700868a2fa36c1287a730a8d2b
dmuvaa <dmuvaa70@gmail.com>
2022-12-19 07:37:04 -0800

    alphas

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,25 @@
 #include "main.h"
+
 /**
  * print_alphabet_x10 - Print alphabets 10 times using the nested loop
  *
  * Return: On success 0
  */
-void print_alphabet_x10(void);
+
+void print_alphabet_x10(void)
 {
 	char i;
-	char j
-	
-	for( i = 'a'; i <= 'j'; i++)
+	int j;
+
+	i = 'a', j = 1;
+
+	for (j = 1; j <= 10; j++)
+	{
+	for (i = 'a'; i <= 'z'; i++)
 	{
 	_putchar(i);
 	}
-	for (j = 10)
 	_putchar('\n')
+	}
+
 }
