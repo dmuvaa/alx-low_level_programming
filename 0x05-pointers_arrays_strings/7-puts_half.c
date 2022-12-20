65cff01e507661d7335f5bf2dbb2c5e5cacc1028
dmuvaa <dmuvaa70@gmail.com>
2022-12-20 14:37:16 -0800

    Print Half String

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints string by half
+ * @str: String to be printed
+ * Description: Print str by half
+ * Return: 0 is success
+ */
+
+void puts_half(char *str)
+{
+	int string = 0, n;
+
+	while (str[string] != '\0')
+		string++;
+	if (string + 1 % 2 != '0')
+		n = (string - 1) / 2;
+	else
+		n = (string / 2);
+	n++;
+
+	for (string = n; str[string] != '\0'; string++)
+		_putchar(str[string]);
+	_putchar('\n');
+}
