efab8b19e6ab24213966828a9eb3db72efb889fe
dmuvaa <dmuvaa70@gmail.com>
2022-12-20 01:53:39 -0800

    print last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+ /**
+  * print_last_digit - prints the last digit of a number
+  * @n: number whose last digits are to be printed;
+  * Return- Value of the last digit of a number
+  */
+
+int print_last_digit(int c)
+{
+	int last;
+
+	last = n % 10;
+	if (last < 0)
+	{
+		last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
+}
