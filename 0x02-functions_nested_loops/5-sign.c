178aabfca44312f5101734f344b4568d871ed0c5
dmuvaa <dmuvaa70@gmail.com>
2022-12-20 01:24:59 -0800

    Print Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,12 +3,12 @@
 /**
  * print_sign - prints the sign of a digit
  * @n: the variable being checked
- * Return: 1 and prints + if n > 0 and prints 0 if n is zero, or -1 and prints - if n < 0
+ * Return: 1 and + if n > 0 and prints 0 if ==0 or -1 and prints - if n < 0
  */
 
 int print_sign(int n)
 {
-	if (n < 0)
+	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
