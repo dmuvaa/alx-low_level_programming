ad02cd2127b21f6b2672b3f12aaf374f28e27b08
dmuvaa <dmuvaa70@gmail.com>
2022-12-20 05:32:12 -0800

    iccanobif

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints the first 98 Fibonacci numbers, starting with
- * 	1 and 2, separated by a comma followed by a space.
+ *1 and 2, separated by a comma followed by a space.
  *
  * Return: 0
  */
