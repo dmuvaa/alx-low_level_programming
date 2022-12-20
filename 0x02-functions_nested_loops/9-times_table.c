c91e48274f04061ba10af40efe8825911fd0edcc
dmuvaa <dmuvaa70@gmail.com>
2022-12-20 03:11:38 -0800

    table times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,33 +7,26 @@
 
 void times_table(void)
 {
-	int x, y, z, u, d;
+	int num, mult, prod;
 
-	for (x = 0; x <= 9; x++)
+	for (num = 0; num <= 9; num++)
 	{
-	for (y = 0; y <= 9; y++)
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
 	{
-	z = x + y;
-	if (z > 9)
-	{
-	u = z % 10;
-	d = (z - u) / 10;
-	_putchar(44);
-	_putchar(32);
-	_putchar(d + '0');
-	_putchar(u + '0');
+		_putchar(',');
+		_putchar(' ');
+
+		prod = num * mult;
+
+		if (prod <= 9)
+			_putchar(' ');
+		else
+			_putchar((prod / 10) + '0');
+
+		_putchar((prod % 10) + '0');
 	}
-	else
-	{
-	if (y != 0)
-	{
-	_putchar(44);
-	_putchar(32);
-	_putchar(32);
-	}
-	_putchar(z + '0');
-	}
-	}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
