c9de754918262d768f399fa31e6fce76307eb74d
dmuvaa <dmuvaa70@gmail.com>
2022-12-20 04:57:30 -0800

    Fibonaccci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,22 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main - list all numbers below 1024 that are multiples of 3 or 5
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2
  *
  * Return: Always 0
  */
 
 int main(void)
 {
-	int i, sum = 0;
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 
-	for (i = 0; i < 1024; i++)
+	for (count = 0; count < 50; count++)
 	{
-		if ((i % 3) == 0 || (i % 5) == 0)
-			sum += i;
-	}
+		sum = fib1 + fib2;
+		printf("%lu", sum);
 
-	printf("%d\n", sum);
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+		printf("\n");
+
+		else
+		printf(", ");
+	}
 
 	return (0);
 }
