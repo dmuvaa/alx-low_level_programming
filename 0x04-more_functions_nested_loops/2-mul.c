ec906e5a602ef3370b5d1e594768d7b6d5a11449
dmuvaa <dmuvaa70@gmail.com>
2023-01-02 13:51:07 -0800

    Function that Multiplies Integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a and @b: integers to be multiplied
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int a;
+	int b;
+
+	{
+		_putchar(a * b);
+			return (0);
+	}
+}
