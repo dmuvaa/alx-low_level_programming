c2c18c6dd0bf942ab1084854de63ae7a7b30561b
dmuvaa <dmuvaa70@gmail.com>
2023-01-06 15:08:45 -0800

    Prime Number Checker

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,7 +2,7 @@
 
 /**
  * is_prime_number - checks if input integer is a prime number
- * n: input integer to be checked
+ * @n: input number to be checked
  * Return: 1 if n is prime number and 0 if otherwise
  */
 
@@ -10,7 +10,7 @@ int is_prime_number(int n)
 {
 	if (n % 2 != 0 && n % 3 != 0)
 		return (1);
-	else if (n <=1)
+	else if (n <= 1)
 		return (-1);
 	else
 		return (0);
