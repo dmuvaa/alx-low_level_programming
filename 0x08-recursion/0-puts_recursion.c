131e72568b0cc82c97a78cd6281e2d1b1640acd0
dmuvaa <dmuvaa70@gmail.com>
2023-01-06 06:34:22 -0800

    Puts Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,14 +8,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
+	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
-
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
