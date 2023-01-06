b28b83ce1f8512ccdac82bc3bea6baf47726344d
dmuvaa <dmuvaa70@gmail.com>
2023-01-06 06:18:19 -0800

    Recurs

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that puts a string
+ * @s: character to be printed
+ * @Return: Returns s
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s == 1)
+		return 1;
+	else
+		return s;
+}
+
+int main(void)
+{
+	_puts_recursion(s);
+	return s;
+
+}
