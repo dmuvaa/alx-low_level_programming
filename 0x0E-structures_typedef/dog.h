e557820b02578e03e871cccaf85f09defe56b278
dmuvaa <dmuvaa70@gmail.com>
2023-01-18 04:27:06 -0800

    Elements of Struct Dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,7 +22,6 @@ struct dog
 	char *owner;
 }
 
-int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
