8f80f87f7a5e5305c5dd2e582c048af02cfb8a56
dmuvaa <dmuvaa70@gmail.com>
2023-01-18 04:23:36 -0800

    Elements of Dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,12 @@
 #define _DOG_H_
 
 /**
+ * dog_t - Typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+/**
  * struct dog - elements for dogs
  * @name: name of dog
  * @age: age of dog
@@ -15,12 +21,6 @@ struct dog
 	float age;
 	char *owner;
 }
-
-/*
- * dog_t - Typedef for struct dog
- */
-
-typedef struct dog dog_t;
 
 int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
