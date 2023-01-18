f62a224287516751bb038cc4962aed33470f5c9b
dmuvaa <dmuvaa70@gmail.com>
2023-01-18 12:49:25 -0800

    Function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog -function that frees dogs
+ * @d: dog to be freed
+ * Returrn: 0 is success
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->age);
+		free(d);
+	}
+}
