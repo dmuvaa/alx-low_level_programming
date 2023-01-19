c59c8428074bde48bd6256725ed16e7be4cd0539
dmuvaa <dmuvaa70@gmail.com>
2023-01-19 11:19:42 -0800

    Function Pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
 
-int _putchar(char);
+#include <stddef.h>
+int _putchar(char c);
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
