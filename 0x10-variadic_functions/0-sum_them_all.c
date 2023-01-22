9fa01e7638a3b42f75114625531ce73c612f5015
dmuvaa <dmuvaa70@gmail.com>
2023-01-22 13:54:51 -0800

    function that returns the sum of all its parameters

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stddarg.h>
+
+/**
+ * sum_them_all - function that returns the sum of all its parameters
+ * @n: first parameter
+ * Return: 0 if n is equal to 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va list n;
+	int b;
+
+	if (n != 0)
+		return sum;
+	else if (n == 0)
+		return (0);
+}
