18941a400cd2bb82ddb39ac1c8b7751c84e5a843
Dennis Muvaa <dmuvaa70@gmail.com>
2023-01-30 09:30:49 -0800

    Lists header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,13 @@
 #ifndef LIST_H
 #define LIST_H
 
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
