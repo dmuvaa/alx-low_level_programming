c9b9e97cc0c7492c97003f7fb6418ea83fac9e57
Dennis Muvaa <dmuvaa70@gmail.com>
2023-02-02 01:55:07 -0800

    Main Header File

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
