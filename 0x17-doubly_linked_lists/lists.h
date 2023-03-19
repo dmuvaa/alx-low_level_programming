47428120f0692d0dbe8f599cfbaaa0358ab1e242
Dennis Muvaa <dmuvaa70@gmail.com>
2023-03-19 13:08:09 -0700

    New Header File

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
