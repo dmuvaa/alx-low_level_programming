a62c5a4ce0e1556188299827af20691a823a2bdb
Dennis Muvaa <dmuvaa70@gmail.com>
2023-04-12 11:03:43 -0700

    function that creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,9 +18,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	array = malloc(sizeof(hash_node_t *) * size);
-	
+
 	if (array == NULL)
-		return (NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		array[i] = NULL;
