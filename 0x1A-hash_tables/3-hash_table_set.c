1226683cb66822d6de0f7a0ca98e92d51a814464
Dennis Muvaa <dmuvaa70@gmail.com>
2023-04-12 12:21:50 -0700

    function that adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,43 +10,46 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	 if (!ht || !key)
-	 {
-		 return (0);
-	 }
+	if (!ht || !key)
+	{
+	return (0);
+	}
 
-	 unsigned long int index = key_index((const unsigned char *)key, ht->size);
-	 hash_node_t *node = ht->array[index];
+	unsigned long int index;
+	hash_node_t *node;
 
-	 while (node)
-	 {
-		 if (strcmp(node->key, key) == 0)
-		 {
-			 free(node->value);
-			 node->value = strdup(value);
-			 return (1);
-		 }
-		 node = node->next;
-	 }
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
 
-	 node = malloc(sizeof(hash_node_t));
-	 if (!node)
-	 {
-		 return (0);
-	 }
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+		free(node->value);
+			node->value = strdup(value);
+			return (1);
+		}
+		node = node->next;
+	}
 
-	 node->key = strdup(key);
-	 node->value = strdup(value);
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+	{
+		return (0);
+	}
 
-	 if (!node->key || !node->value)
-	 {
-		 free(node->key);
-		 free(node->value);
-		 free(node);
-		 return (0);
-	 }
-	 node->next = ht->array[index];
-	 ht->array[index] = node;
+	node->key = strdup(key);
+	node->value = strdup(value);
 
-	 return (1);
+	if (!node->key || !node->value)
+	{
+		free(node->key);
+		free(node->value);
+		free(node);
+		return (0);
+	}
+	node->next = ht->array[index];
+	ht->array[index] = node;
+
+	return (1);
 }
