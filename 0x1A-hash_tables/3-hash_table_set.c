387bb3ef4a5f5599e9d63a25a93708e8dd5024d3
Dennis Muvaa <dmuvaa70@gmail.com>
2023-04-12 13:02:30 -0700

    function that adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -35,26 +35,47 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	return (1);
 }
-/
 
-	index = key_index((const unsigned char *)key, ht->size);
-	node = ht->array[index];
+/**
+ * update_node_value - updates the value of a node if the key matches
+ * @node: pointer to the node in the hash table
+ * @key: key to be matched
+ * @value: new value to be set
+ *
+ * Return: 1 if the value is updated, 0 otherwise
+ */
 
+int update_node_value(hash_node_t *node, const char *key, const char *value)
+{
 	while (node)
 	{
 		if (strcmp(node->key, key) == 0)
 		{
-		free(node->value);
+			free(node->value);
 			node->value = strdup(value);
 			return (1);
 		}
+
 		node = node->next;
 	}
 
-	node = malloc(sizeof(hash_node_t));
+	return (0);
+}
+
+/**
+ * create_new_node - creates a new hash table node with the given key and value
+ * @key: key for the new node
+ * @value: value for the new node
+ *
+ * Return: pointer to the new node, NULL if memory allocation fails
+ */
+
+hash_node_t *create_new_node(const char *key, const char *value)
+{
+	hash_node_t *node = malloc(sizeof(hash_node_t));
 	if (!node)
 	{
-		return (0);
+		return (NULL);
 	}
 
 	node->key = strdup(key);
@@ -65,10 +86,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(node->key);
 		free(node->value);
 		free(node);
-		return (0);
+		return (NULL);
 	}
-	node->next = ht->array[index];
-	ht->array[index] = node;
 
-	return (1);
+	return (node);
 }
