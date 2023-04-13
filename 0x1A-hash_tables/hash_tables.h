12161c8dd392a4a95c18e8acbff52ee48568e173
Dennis Muvaa <dmuvaa70@gmail.com>
2023-04-13 02:34:20 -0700

    Main header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -81,8 +81,11 @@ typedef struct shash_table_s
      shash_node_t *stail;
 } shash_table_t;
 
-static shash_node_t *shash_node_create(const char *key, const char *value);
-static void shash_sorted_insert(shash_table_t *ht, shash_node_t *new_node);
-static void shash_sorted_delete(shash_table_t *ht);
+shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
 
 #endif
