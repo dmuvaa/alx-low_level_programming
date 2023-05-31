68f1ab0b6f3ed00cf5dd0da761dda86f273bfadd
Dennis Muvaa <dmuvaa70@gmail.com>
2023-05-31 08:14:16 -0700

    New Node

--- binary_trees/0-binary_tree_node.c
+++ binary_trees/0-binary_tree_node.c
@@ -7,7 +7,7 @@
  * @Return: pointer to the new node, or NULL
  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
